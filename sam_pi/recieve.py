882511c0ba7f2ba487905f79b39ec22cdc0a7176
Pato <p.pato.otap@gmail.com>
2018-01-30 17:53:31 +0100

    Initialize repository

---
+++ sam_pi/recieve.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf8 -*-
+
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      Osnovnoy
+#
+# Created:     30/01/2018
+# Copyright:   (c) Osnovnoy 2018
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+import requests
+import json
+import urllib2
+
+class Recieve:
+
+    def __init__(self,uid):
+        content = uid
+        response = requests.get('http://aberdeen-sam.herokuapp.com/')
+        data = json.load(response.content)
