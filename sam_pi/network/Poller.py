49c0fbeac522801f212b712dcc68744857c77215
Pattio <Pattio@users.noreply.github.com>
2018-03-29 15:28:40 +0100

    Update Poller.py

--- sam_pi/network/Poller.py
+++ sam_pi/network/Poller.py
@@ -10,41 +10,41 @@ class Poller:
     fingerprint_pipe = None
     pi_id = None
     
-    # Todo pass fingeprint class as parameter as well
+    # Initialize parser, fingerprint_pipe and pi_id
     def __init__(self, parser, fingerprint_pipe, pi_id):
         self.parser = parser
         self.fingerprint_pipe = fingerprint_pipe
         self.pi_id = pi_id
     
+    # Start the polling process
     def startPolling(self):
         while 1:
-            print("poll")
             self.pollPendingPracticals()
             self.pollCurrentTemplates()
             
-     
+    # Polls practicals that are pending
     def pollPendingPracticals(self):
         if self.parser.course_id == None:
+            # Try to get pending practicals
             pending_practical = self.parser.query_pending_practicals(self.pi_id)
             if pending_practical.error != None:
                 print("There was an error:" + pending_practical.error)
             elif pending_practical.pending:
                 print("Practical for course: " + pending_practical.course_id + " started")
                 LED.asyncGreen()
-                #LCD.passmessage("COURSE ID " + pending_practical.course_id + "                        INITIALIZED")
                 # Inform fingerprint about practical
                 self.fingerprint_pipe.send(pending_practical.course_id)
+        # Wait 10 seconds
         time.sleep(10)
         
     def pollCurrentTemplates(self):
+        # Only poll for fingerprint templates, if practical is not started
         if self.parser.course_id == None:
+            # Try to get fingerprint templates
             upcoming_templates = self.parser.current_templates()
             if upcoming_templates.templates != None:
                 # Load templates to fingerprint
-                # it would be cool to have a variable inside fingeprint sensor which would tell
-                # when was the last time templates were loaded
-                # if templates were loaded recently do not call parser.current_templates method
-                # i.e add check to first if statement
                 self.fingerprint_pipe.send(upcoming_templates.templates)
+        # Wait 60 seconds
         time.sleep(60)
         
